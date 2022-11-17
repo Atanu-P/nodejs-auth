565f690e0cb367c9b807ca33147f0ef2887ad2a9
Atanu Pal <atanupal@live.com>
2022-11-17 13:23:52 +0530

    user schema and model created

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 
+// express app
 const app = express();
 
+// connect to mongo db
 const db_url =
   "mongodb+srv://node:node1234@test.7ydnwmm.mongodb.net/node_auth?retryWrites=true&w=majority";
-
 mongoose
   .connect(db_url)
   .then((result) => {
@@ -21,3 +22,12 @@ app.set("view engine", "ejs");
 // middleware
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
+
+// route
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/profile", (req, res) => {
+  res.render("profile");
+});
